b3414bfa07552799f84aa45f3bbabd977559a4f2
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2023-07-13 22:28:04 +0900

    Feat : 탐색창, 알리미창 1차 UI

---
+++ pochak/pochak/Alarm/AlarmViewController.swift
@@ -0,0 +1,99 @@
+//
+//  AlarmViewController.swift
+//  pochak
+//
+//  Created by 장나리 on 2023/07/11.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Pretendard-bold", size: 20)!]
+        self.navigationItem.title = "알리미"
+        
+        // Do any additional setup after loading the view.
+        setupCollectionView()
+
+    }
+    private func setupCollectionView(){
+        //delegate 연결
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        //cell 등록
+        collectionView.register(UINib(
+            nibName: "AlarmCollectionViewCell",
+            bundle: nil),
+            forCellWithReuseIdentifier: AlarmCollectionViewCell.identifier)
+        
+        collectionView.register(UINib(
+            nibName: "OtherCollectionViewCell",
+            bundle: nil),
+            forCellWithReuseIdentifier: OtherCollectionViewCell.identifier)
+    }
+
+    
+    private func setupData(){
+//        UserFeedDataManager().getUserFeed(self)
+    }
+
+}
+extension AlarmViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10 //아이템 개수
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let section = indexPath.section
+        switch section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlarmCollectionViewCell.identifier, for: indexPath) as? AlarmCollectionViewCell else{
+                fatalError("셀 타입 캐스팅 실패")
+            }
+            return cell
+        default:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OtherCollectionViewCell.identifier, for: indexPath) as? OtherCollectionViewCell else{
+                fatalError("셀 타입 캐스팅 실패2")
+            }
+            //                let itemIndex = indexPath.item
+            //                if let cellData = self.userPosts{
+            //                    // 데이터가 있는 경우, cell 데이터를 전달
+            //                    cell.setupData(cellData[itemIndex].postImgUrl)
+            //                }
+            // <- 데이터 전달
+            return cell
+        }
+    }
+}
+    
+    extension AlarmViewController: UICollectionViewDelegateFlowLayout {
+        
+        // 위 아래 간격
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 1
+        }
+        
+        // 옆 간격
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+            return 1
+        }
+        
+        // cell 사이즈( 옆 라인을 고려하여 설정 )
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            
+            let width = collectionView.frame.width
+            
+            let size = CGSize(width: width, height: width)
+            return size
+        }
+    }
+
--- pochak/pochak/PostTabViewController.swift
+++ pochak/pochak/PostTabViewController.swift
@@ -8,32 +8,138 @@
 import UIKit
 
 class PostTabViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupCollectionView()
+
+        let searchController = UISearchController(searchResultsController: nil)
+        self.navigationItem.searchController = searchController
+        
 
         // Do any additional setup after loading the view.
     }
+    private func setupCollectionView(){
+        //delegate 연결
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        //cell 등록
+        collectionView.register(UINib(
+            nibName: "PostCollectionViewCell",
+            bundle: nil),forCellWithReuseIdentifier: PostCollectionViewCell.identifier)
+        
+    }
     
-    @IBAction func viewPostBtnTapped(_ sender: Any) {
+    private func setupData(){ // 서버 연결 시
+        //        UserFeedDataManager().getUserFeed(self)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    @objc private func cancelBtnAction(_ sender: Any){ // 취소 버튼 클릭시 텍스트 삭제? 키보드 내리기? 어떤 action 할건지
+        
+    }
+}
+    
+extension PostTabViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 2
+        default:
+            return 24
+            
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let section = indexPath.section
+        switch section {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostCollectionViewCell.identifier, for: indexPath) as? PostCollectionViewCell else{
+                fatalError("셀 타입 캐스팅 실패")
+            }
+            return cell
+        default:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostCollectionViewCell.identifier, for: indexPath) as? PostCollectionViewCell else{
+                fatalError("셀 타입 캐스팅 실패2")
+            }
+            //                let itemIndex = indexPath.item
+            //                if let cellData = self.userPosts{
+            //                    // 데이터가 있는 경우, cell 데이터를 전달
+            //                    cell.setupData(cellData[itemIndex].postImgUrl)
+            //                }
+            // <- 데이터 전달
+            return cell
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("view post btn tapped")
         guard let postVC = self.storyboard?.instantiateViewController(withIdentifier: "PostVC") as? PostViewController else { return }
         print(postVC)
         self.navigationController?.pushViewController(postVC, animated: true)
-//        let sb = UIStoryboard(name: "PostTab", bundle: nil)
-//        let postVC = sb.instantiateViewController(withIdentifier: "PostVC") as! PostViewController
-//        postVC.modalPresentationStyle = .fullScreen
-//        self.present(postVC, animated: true, completion: nil)
+        //        let sb = UIStoryboard(name: "PostTab", bundle: nil)
+        //        let postVC = sb.instantiateViewController(withIdentifier: "PostVC") as! PostViewController
+        //        postVC.modalPresentationStyle = .fullScreen
+        //        self.present(postVC, animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+        
+extension PostTabViewController: UICollectionViewDelegateFlowLayout {
+    
+    // 위 아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        switch section {
+            
+        case 0:
+            return CGFloat(0)
+        default:
+            return CGFloat(4)
+        }
+    }
+
+        
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets(top: 2, left: 1, bottom: 2, right: 1)
+        }
+    // 옆 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        switch section {
+            case 0:
+                return CGFloat(0)
+            default:
+                return CGFloat(1)
+            }
+
+        }
+
+            
+    // cell 사이즈( 옆 라인을 고려하여 설정 )
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let section = indexPath.section
+        switch section {
+        case 0:
+            return CGSize(width: 168, height: 230)
+        default:
+            return CGSize(width: 111, height: 128)
+        }
+    }
+}
+
