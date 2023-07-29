8d5b8955505fdb8f07399a7a19a0d6dd0a9626da
Suyeon <syhhwang1231@gmail.com>
2023-07-29 17:53:54 +0900

    Fix: 댓글 내용 UItextview로 변경
    - 아이디 태그 기능 위해서

--- pochak/pochak/Post/Comment/cell/CommentTableViewCell.swift
+++ pochak/pochak/Post/Comment/cell/CommentTableViewCell.swift
@@ -9,19 +9,39 @@ import UIKit
 
 class CommentTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var commentLabel: UILabel!
+    // MARK: - Properties
+    @IBOutlet weak var commentTextView: MentionTextView!
     @IBOutlet weak var profileImageView: UIImageView!
     
+    var taggedId: String = ""
+    
+    // MARK: - Action
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        /* commentTextView 초기화 */
+        print("tableviewcell")
+        
+        // commentTextView의 inset 제거
+        commentTextView.textContainerInset = .zero
+        commentTextView.textContainer.lineFragmentPadding = 0
+        
+        
+//        commentTextView.delegate = (self.textView(commentTextView, shouldInteractWith: <#T##URL#>, in: <#T##NSRange#>, interaction: <#T##UITextItemInteraction#>) as! any UITextViewDelegate)
+        
+        // commentTextView에서 아이디 찾기
+        commentTextView.findOutMetionedId()
+        
+        commentTextView.delegate = self
         
         // 크기 반만큼 radius
         profileImageView.layer.cornerRadius = 17.5
         
-        // 사용자(아이디로) 멘션 기능
-        commentLabel.findOutMentionedId()
+        // 사용자(아이디로) 멘션 기능 (댓글에서 아이디 탐지)
+        //taggedId = commentLabel.findOutMentionedId()
         
+        // label이 터치 인식할 수 있도록 gesture recognizer 추가
+        //let recognizer = UITapGestureRecognizer(target: self, action: #selector(taggedIdTapped))
+        //commentLabel.addGestureRecognizer(recognizer)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -30,4 +50,18 @@ class CommentTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+}
+
+// commentTextView를 위한 delegate
+extension CommentTableViewCell: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldInteractWith url: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        
+        if let mentionTextView = textView as? MentionTextView,
+           let text = Int(url.debugDescription) {
+            print(mentionTextView.idArray[text])
+            return false
+        }
+        return false
+    }
 }
--- pochak/pochak/Post/Comment/cell/ReplyTableViewCell.swift
+++ pochak/pochak/Post/Comment/cell/ReplyTableViewCell.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ReplyTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var replyCommentTextView: UITextView!
     @IBOutlet weak var profileImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // replyCommentTextView의 inset 제거
+        replyCommentTextView.textContainerInset = .zero
+        replyCommentTextView.textContainer.lineFragmentPadding = 0
         
         // 이미지뷰 반만큼 radius 적용 -> 동그랗게
         profileImageView.layer.cornerRadius = 10
