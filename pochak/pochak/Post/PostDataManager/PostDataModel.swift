6f476ea1f3b9187767982e61c845130f44b0c8f4
Suyeon <syhhwang1231@gmail.com>
2023-11-18 08:21:53 -0600

    Feat: 포스트 상세 페이지 API 연결 실패..
    - success 로 빠지나 데이터 추출에서 실패

--- pochak/pochak/Post/PostDataManager/PostDataModel.swift
+++ pochak/pochak/Post/PostDataManager/PostDataModel.swift
@@ -5,19 +5,19 @@
 //  Created by Suyeon Hwang on 11/10/23.
 //
 
-struct PostDataReponse: Decodable {
-    let isSuccess: Bool?
-    let code: Int?
-    let message: String?
-    let result: PostDataResponseResult?
-}
-
 struct PostDataResponseResult: Decodable {
     let taggedUserHandles: [String]?
-    let postOwnerhandle: String?
+    let postOwnerHandle: String?
     let isFollow: Bool?
     let postImageUrl: String?
     let numOfHeart: Int?
     let caption: String?
     let mainComment: String?
+}
+
+struct PostDataReponse: Decodable {
+    let isSuccess: Bool?
+    let code: Int?
+    let message: String?
+    let result: PostDataResponseResult?
 }
