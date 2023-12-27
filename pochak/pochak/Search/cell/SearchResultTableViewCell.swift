17ecf83ab7abec357f4e06c9d1aff425fe0832f1
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2023-12-27 22:13:43 +0900

    Feat: 탐색 탭 검색 뷰

--- pochak/pochak/Search/cell/SearchResultTableViewCell.swift
+++ pochak/pochak/Search/cell/SearchResultTableViewCell.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class SearchResultTableViewCell: UITableViewCell {
-
+    static let identifier = "SearchResultTableViewCell"
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var profileImg: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupAttribute()
         // Initialization code
     }
 
+    private func setupAttribute(){
+        profileImg.layer.cornerRadius = 50/2
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
