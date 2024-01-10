862837d49b113b38b244b77bf5a03036d61e7a27
Suyeon <syhhwang1231@gmail.com>
2024-01-10 22:11:43 +0900

    Fix: dxxynni 헤더로 변경

--- pochak/pochak/Post/LikesDataManager/LikedUsersDataService.swift
+++ pochak/pochak/Post/LikesDataManager/LikedUsersDataService.swift
@@ -50,7 +50,7 @@ struct LikedUsersDataService{
     
     func postLikeRequest(_ postId: String, completion: @escaping (NetworkResult<Any>) -> Void){
         /* header 있는 자리 */
-        let header : HTTPHeaders = ["Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaXNvbyIsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE2OTkwOTMzNTIsImV4cCI6MTc3Njg1MzM1Mn0.8Cz-E0OmD8aK9wC8YApk1JenueXM86O9lPH0_pUcnLc",
+        let header : HTTPHeaders = ["Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkeHh5bm5pIiwicm9sZSI6IlJPTEVfVVNFUiIsImlhdCI6MTcwMzY5MDExNywiZXhwIjoxNzgxNDUwMTE3fQ.2kaatfaOOZeor-RrK09ZCBaxizKI8KGs14Pt-j_uuoU",
                                             "Content-type": "application/json"  // multipart/form-data ???
                                             ]
         let dataRequest = AF.request(APIConstants.baseURL+"/api/v1/post/"+postId+"/like",
