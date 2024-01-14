40b3e45c07a1ece6472860448ebe96265066a1d1
Suyeon <syhhwang1231@gmail.com>
2024-01-15 02:32:55 +0900

    Fix: 대댓글 셀 - 대댓글 등록 시간 계산해서 보여주기

--- pochak/pochak/Post/Comment/cell/ReplyTableViewCell.swift
+++ pochak/pochak/Post/Comment/cell/ReplyTableViewCell.swift
@@ -59,7 +59,47 @@ class ReplyTableViewCell: UITableViewCell {
         
         replyCommentTextView.text = commentData.content
         
-        timePassedLabel.text = commentData.uploadedTime
+        // comment.uploadedTime 값: 2023-12-27T19:03:32.701
+        // 시간 계산
+        let arr = commentData.uploadedTime.split(separator: "T")  // T를 기준으로 자름, ["2023-12-27", "19:03:32.701"]
+        let timeArr = arr[arr.endIndex - 1].split(separator: ".")  // ["19:03:32", "701"]
+        
+        let uploadedTime = arr[arr.startIndex] + " " + timeArr[timeArr.startIndex]
+        
+        // 현재 시간
+        let currentTime = Date()
+        
+        let format = DateFormatter()
+        format.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let startTime = format.date(from: String(uploadedTime))!
+        //let startStr = format.string(from: startTime!)
+        let endStr = format.string(from: currentTime)
+        let endTime = format.date(from: endStr)
+        
+        let timePassed = Int(endTime!.timeIntervalSince(startTime))  // 초단위 리턴
+        // 초
+        if(timePassed >= 0 && timePassed < 60){
+            self.timePassedLabel.text = String(timePassed) + "초"
+        }
+        // 분
+        else if(timePassed >= 60 && timePassed < 3600){
+            self.timePassedLabel.text = String(timePassed / 60) + "분"
+        }
+        // 시
+        else if(timePassed >= 3600 && timePassed < 24*60*60){
+            self.timePassedLabel.text = String(timePassed / 3600) + "시간"
+        }
+        
+        // 일
+        else if(timePassed >= 24*60*60 && timePassed < 7*24*60*60){
+            self.timePassedLabel.text = String(timePassed/(24*60*60)) + "일"
+        }
+        
+        // 주
+        else{
+            self.timePassedLabel.text = String(timePassed / (7*24*60*60)) + "주"
+        }
     }
 }
 
