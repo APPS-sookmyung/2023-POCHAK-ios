57c7036363e5ccb22a3f67fb5fe3f54d052836cf
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2024-01-16 21:38:15 +0900

    feat: 최근검색어 저장

--- pochak/pochak/Search/cell/SearchResultTableViewCell.swift
+++ pochak/pochak/Search/cell/SearchResultTableViewCell.swift
@@ -13,11 +13,17 @@ class SearchResultTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var userHandle: UILabel!
     @IBOutlet weak var deleteBtn: UIImageView!
+    
     static let identifier = "SearchResultTableViewCell"
+    
+    var deleteButtonAction: (() -> Void)?
+
     override func awakeFromNib() {
         super.awakeFromNib()
         setupAttribute()
         // Initialization code
+        print("SearchResultTableViewCell")
+        imageViewClickGesture()
     }
 
     private func setupAttribute(){
@@ -28,6 +34,21 @@ class SearchResultTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    private func imageViewClickGesture(){
+        // UITapGestureRecognizer 생성
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped))
+
+        // 이미지뷰에 UITapGestureRecognizer 추가
+        deleteBtn.addGestureRecognizer(tapGesture)
+
+        // 사용자 상호 작용을 가능하게 하려면 반드시 다음을 설정해야 합니다.
+        deleteBtn.isUserInteractionEnabled = true
+    }
+
+    @objc func imageViewTapped() {
+           deleteButtonAction?()
     }
     
     // 이미지 설정 함수
