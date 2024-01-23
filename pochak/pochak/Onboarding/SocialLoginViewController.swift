897cb513f849fe02529397076dcc19c8c47843f3
Jungyeon Seo <seocindy2002@gmail.com>
2024-01-23 17:53:49 +0900

    Fix : 로그아웃, 회원탈퇴 구현 완료

--- pochak/pochak/Onboarding/SocialLoginViewController.swift
+++ pochak/pochak/Onboarding/SocialLoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class SocialLoginViewController: UIViewController {
     
@@ -44,6 +45,72 @@ class SocialLoginViewController: UIViewController {
         // Hide Back Button
         self.navigationItem.setHidesBackButton(true, animated: true)
 
+    }
+    
+    // MARK: - Google Login
+    @IBAction func googleLoginAction(_ sender: Any) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+            guard let signInResult = signInResult else { return }
+            
+            // Get User Info
+            let user = signInResult.user
+            let accessToken = user.accessToken.tokenString
+            GoogleLoginDataManager.shared.googleLoginDataManager(accessToken, {resultData in
+                guard let isNewMember = resultData.isNewMember else { return }
+                guard let name = resultData.name else { return }
+                guard let email = resultData.email else { return }
+                guard let socialType = resultData.socialType else { return }
+                guard let socialId = resultData.id else { return }
+                
+                // 사용자 기본 데이터 저장
+                UserDefaultsManager.setData(value: socialId, key: .socialId)
+                UserDefaultsManager.setData(value: email, key: .email)
+                UserDefaultsManager.setData(value: socialType, key: .socialType)
+                UserDefaultsManager.setData(value: isNewMember, key: .isNewMember)
+                print(isNewMember)
+                
+                self.changeViewControllerAccordingToisNewMemeberState(isNewMember, resultData)
+            })
+        }
+    }
+    
+    // MARK: - profileSettingPage or HomeTabPage로 전환하기
+    private func changeViewControllerAccordingToisNewMemeberState(_ isNewMember : Bool, _ resultData : GoogleLoginModel){
+        if isNewMember == true {
+            // 알람! : 회원정보가 없습니다 회원가입하시겠습니까?
+            let alert = UIAlertController(title:"이런..회원정보가 없어요! 회원가입하시겠습니까?",message: "",preferredStyle: UIAlertController.Style.alert)
+            let cancle = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            let ok = UIAlertAction(title: "확인", style: .default, handler: {
+                action in
+                self.toMakeProfilePage()
+            })
+            alert.addAction(cancle)
+            alert.addAction(ok)
+            present(alert,animated: true,completion: nil)
+        } else {
+            // 토큰 정보 저장 @KeyChainManager
+            guard let accountAccessToken = resultData.accessToken else { return }
+            guard let accountRefreshToken = resultData.refreshToken else { return }
+            do {
+                try KeychainManager.save(account: "accessToken", value: accountAccessToken, isForce: true)
+                try KeychainManager.save(account: "refreshToken", value: accountRefreshToken, isForce: true)
+            } catch {
+                print(error)
+            }
+            // 홈탭으로 이동
+            toHomeTabPage()
+        }
+    }
+    
+    private func toHomeTabPage(){
+        guard let tabbarVC = self.storyboard?.instantiateViewController(withIdentifier: "TabbarVC") as? TabbarController else { return }
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(tabbarVC, animated: false)
+    }
+    
+    private func toMakeProfilePage(){
+        guard let makeProfileVC = self.storyboard?.instantiateViewController(withIdentifier: "MakeProfileVC") as? MakeProfileViewController else {return}
+        self.navigationController?.pushViewController(makeProfileVC, animated: true)
     }
     
     // MARK: - Back Button Action
--- pochak/pochak/Profile/MyProfile/MyProfilePostDataManager/MyProfilePostDataManager.swift
+++ pochak/pochak/Profile/MyProfile/MyProfilePostDataManager/MyProfilePostDataManager.swift
@@ -15,69 +15,68 @@ class MyProfilePostDataManager {
     let accessToken = GetToken().getAccessToken()
     let refreshToken = GetToken().getRefreshToken()
     
-    func myProfilePochakedPostDataManager(_ handle : String, _ completion: @escaping ([PostDataModel]) -> Void) {
-        let url = APIConstants.baseURL + "/api/v1/user/profile/" + handle + "/pochak"
-
-        let header : HTTPHeaders = ["Authorization": accessToken, "Content-type": "application/json"]
-        
-        // Load Authenticator
-        let authenticator = MyAuthenticator()
-        let credential = MyAuthenticationCredential(accessToken: GetToken().getAccessToken(),
-                                                        refreshToken: GetToken().getRefreshToken(),
-                                                        expiredAt: Date(timeIntervalSinceNow: 60 * 120))
-        let myAuthencitationInterceptor = AuthenticationInterceptor(authenticator: authenticator,
-                                                                        credential: credential)
-
-        AF.request(url,
-                   method: .get,
-                   encoding: URLEncoding.default,
-                   headers: header,
-                   interceptor: myAuthencitationInterceptor)
-        .validate()
-        .responseDecodable(of: MyProfilePochakedPostResponse.self) { response in
-               switch response.result {
-               case .success(let result):
-                   print("myProfilePochakedPostDataManager")
-                   let resultData = result.result.uploadPosts
-                   completion(resultData)
-               case .failure(let error):
-                   print("myProfilePochakedPostDataManager")
-                   print(error)
-               }
-           }
-    }
-    
     func myProfilePochakPostDataManager(_ handle : String, _ completion: @escaping (MyProfilePochakPostModel) -> Void) {
-        print(refreshToken)
         print(accessToken)
+        print(handle)
         let url = APIConstants.baseURL + "/api/v1/user/profile/" + handle
         
         let header : HTTPHeaders = ["Authorization": accessToken, "Content-type": "application/json"]
         
         // Load Authenticator
-        let authenticator = MyAuthenticator()
-        let credential = MyAuthenticationCredential(accessToken: GetToken().getAccessToken(),
-                                                        refreshToken: GetToken().getRefreshToken(),
-                                                        expiredAt: Date(timeIntervalSinceNow: 60 * 120))
-        let myAuthencitationInterceptor = AuthenticationInterceptor(authenticator: authenticator,
-                                                                        credential: credential)
+//        let authenticator = MyAuthenticator()
+//        let credential = MyAuthenticationCredential(accessToken: accessToken,
+//                                                    refreshToken: refreshToken,
+//                                                    expiredAt: Date(timeIntervalSinceNow: 60 * 60))
+//        let myAuthencitationInterceptor = AuthenticationInterceptor(authenticator: authenticator,
+//                                                                    credential: credential)
         
         AF.request(url,
                    method: .get,
                    encoding: URLEncoding.default,
-                   headers: header,
-                   interceptor: myAuthencitationInterceptor)
+                   headers: header
+                   /*interceptor: myAuthencitationInterceptor*/)
         .validate()
         .responseDecodable(of: MyProfilePochakPostResponse.self) { response in
-               switch response.result {
-               case .success(let result):
-                   let resultData = result.result
-                   print("myProfilePochakPostDataManager")
-                   completion(resultData)
-               case .failure(let error):
-                   print("myProfilePochakPostDataManager")
-                   print(error)
-               }
-           }
+            print(response)
+            switch response.result {
+            case .success(let result):
+                let resultData = result.result
+                completion(resultData)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func myProfilePochakedPostDataManager(_ handle : String, _ completion: @escaping ([PostDataModel]) -> Void) {
+        let url = APIConstants.baseURL + "/api/v1/user/profile/" + handle + "/pochak"
+        
+        let header : HTTPHeaders = ["Authorization": accessToken, "Content-type": "application/json"]
+        
+        // Load Authenticator
+//        let authenticator = MyAuthenticator()
+//        let credential = MyAuthenticationCredential(accessToken: GetToken().getAccessToken(),
+//                                                    refreshToken: GetToken().getRefreshToken(),
+//                                                    expiredAt: Date(timeIntervalSinceNow: 60 * 60))
+//        let myAuthencitationInterceptor = AuthenticationInterceptor(authenticator: authenticator,
+//                                                                    credential: credential)
+        
+        AF.request(url,
+                   method: .get,
+                   encoding: URLEncoding.default,
+                   headers: header
+                   /*interceptor: myAuthencitationInterceptor*/)
+        .validate()
+        .responseDecodable(of: MyProfilePochakedPostResponse.self) { response in
+            switch response.result {
+            case .success(let result):
+                print("myProfilePochakedPostDataManager")
+                let resultData = result.result.uploadPosts
+                completion(resultData)
+            case .failure(let error):
+                print("myProfilePochakedPostDataManager")
+                print(error)
+            }
+        }
     }
 }
